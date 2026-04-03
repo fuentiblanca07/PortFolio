2881bcba6c14a60a015b5965d3d5b92c5b22b462
fuentiblanca07 <98931394+kousei1@users.noreply.github.com>
2026-04-03 19:30:44 +0800

    try

--- resources/js/main.jsx
+++ resources/js/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import App from "./app";
 import "../css/tailwind.css";
 import "../css/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/*" element={<App />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
